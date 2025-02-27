c1ca902121000d07b93d2fd5b6bd640f81066954
theotrin <theopinheiro12@gmail.com>
2025-02-27 14:45:47 -0400

    refactor: `Program.cs` refatorado e limpo

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<PdfTextExtractorService>();
 
 var app = builder.Build();
 
@@ -23,38 +24,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
-
-
-
-
-EmbeddingService embeddingService = new EmbeddingService();
-//  var embedding = await embeddingService.GenerateEmbeddingAsync("Hello, world!");
-//  Console.WriteLine($"Embedding: {string.Join(", ", embedding)}");
-
-PdfTextExtractorService pdfTextExtractorService = new PdfTextExtractorService();
-ProprocessTextService proprocessTextService = new ProprocessTextService();
-
-var text = pdfTextExtractorService.ExtractTextFromPdf("Services/Profile.pdf");
-
-List<string> chunks = proprocessTextService.TextSplit(text);
-
-// foreach (var chunk in chunks)
-// {
-//     Console.WriteLine(chunk);
-// }
-
-  List<float[]> embeddings = new List<float[]>();
-  foreach (var chunk in chunks) 
-  {
-    var embedding = await embeddingService.GenerateEmbeddingAsync(chunk);
-    
-    float[] embeddingArray = embedding.ToArray();
-    
-    embeddings.Add(embeddingArray);
-    Console.WriteLine($"Generated embedding for chunk of length {chunk.Length}");
-    Console.WriteLine($"Embedding: {string.Join(", ", embeddingArray)}");
-    Console.WriteLine($"Chunk: {chunk}");
-  }
 
 app.Run();
