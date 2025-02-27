aac96e58856ed38d4b4a44f5b918655f0bfb1741
theotrin <theopinheiro12@gmail.com>
2025-02-27 16:30:29 -0400

    feat: `Program.cs` refatorado e agora sem erro de CORS

--- Program.cs
+++ Program.cs
@@ -1,28 +1,34 @@
 using StartApi.Services;
+using Microsoft.AspNetCore.Builder;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Adiciona serviços
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<PdfTextExtractorService>();
+builder.Services.AddScoped<ProprocessTextService>();
+
+// Configuração de CORS simples
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()    // Permite qualquer origem
+              .AllowAnyMethod()    // Permite qualquer método (POST, GET, etc.)
+              .AllowAnyHeader();   // Permite qualquer cabeçalho
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
+// Configuração do pipeline
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseStaticFiles(); // Para servir arquivos estáticos, se necessário
+app.UseCors("AllowAll"); // Aplica a política de CORS antes das rotas
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
