5ff542404f4d757d0ff375cd1242abd84c0e4788
theotrin <theopinheiro12@gmail.com>
2025-03-10 17:42:23 -0400

    feat: adcionado controller `AskQuestion` para processar e enviar a pergunta do usuario via API

--- Controllers/PdfController.cs
+++ Controllers/PdfController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StartApi.Models;
 using StartApi.Services;
 
 
@@ -14,11 +15,13 @@ namespace ragproject.Controllers
     {
     private readonly PdfTextExtractorService _pdfTextExtractorService;
     private readonly ProprocessTextService _proprocessTextService;
+    private readonly QuestionProcessingService _questionProcessingService;
 
-        public PdfController(PdfTextExtractorService pdfTextExtractorService, ProprocessTextService proprocessTextService)
+        public PdfController(PdfTextExtractorService pdfTextExtractorService, ProprocessTextService proprocessTextService, QuestionProcessingService questionProcessingService)
         {
             _pdfTextExtractorService = pdfTextExtractorService;
             _proprocessTextService = proprocessTextService;
+            _questionProcessingService = questionProcessingService;
         }
 
         [HttpGet("GetPdf")]
@@ -52,6 +55,30 @@ namespace ragproject.Controllers
         catch (Exception ex)
         {
             return StatusCode(500, $"Erro ao processar o PDF: {ex.Message}");
+        }
+    }
+
+    [HttpPost("ask-question")]
+    public IActionResult AskQuestion([FromBody] AskQuestionRequest request)
+    {
+        try
+        {
+            var response = _questionProcessingService.ProcessQuestion(request.Question, request.ProductLabel);
+            return Ok(new
+            {
+                message = "Pergunta processada com sucesso!",
+                question = request.Question,
+                productLabel = request.ProductLabel,
+                response
+            });
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new { error = "Erro ao processar a pergunta", details = ex.Message });
         }
     }
 }
